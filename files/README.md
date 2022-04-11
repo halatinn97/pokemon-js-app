6feeca289fa6277edf4910caf675003fb4a12beb
Hala Tinn <hala79tinn@gmail.com>
2022-04-11 20:31:33 +0200

    Updated js file & README.md
    Built rough first structure of the data set in js.

--- files/README.md
+++ files/README.md
@@ -1,1 +1,13 @@
-# pokemon-js-app
+# Pokédex-App
+
+- Project description: The  app displays a list of different Pokémons and their
+characteristics. This project is an app built with HTML, CSS, and JavaScript,
+which uses an external API to load data and enables the viewing of data
+points in detail.
+
+- How to get the project running: [Visit website] (https TBA) - works in all major browsers: Chrome,
+Firefox, Safari, Microsoft Edge
+
+- Project dependencies: JavaScript ES6, ESLint rules, Prettier
+
+- APIs: External
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
-alert('Hello world');
-let favoriteFood = 'chicken';
-document.write(favoriteFood);
+//Array will contain Pokemon data to display in application
+let pokemonList = [];
+
+//Each object represents one Pokemon with respective properties
+pokemons = [
+        {name:'Pikachu', height: 0.4, type:['electric','ground','steel','flying']},
+        {name:'Charmeleon', height: 1.1, type:['ground','rock', 'water']},
+        {name: 'Slowbro' ,height: 1.6, type:['electric', 'grass','dragon','ghost','bug']}
+      ];

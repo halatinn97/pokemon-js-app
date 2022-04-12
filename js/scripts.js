3d696398d065b6d57572e46696c6a78f7aa840a4
Hala Tinn <hala79tinn@gmail.com>
2022-04-12 06:54:00 +0200

    Updated scripts.js
    Removed empty array from rough draft and assigned first array of objects to var.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
-//Array will contain Pokemon data to display in application
-let pokemonList = [];
-
+//Array contains Pokemon data to display in application
 //Each object represents one Pokemon with respective properties
-pokemons = [
+let pokemonList = [
         {name:'Pikachu', height: 0.4, type:['electric','ground','steel','flying']},
         {name:'Charmeleon', height: 1.1, type:['ground','rock', 'water']},
         {name: 'Slowbro' ,height: 1.6, type:['electric', 'grass','dragon','ghost','bug']}

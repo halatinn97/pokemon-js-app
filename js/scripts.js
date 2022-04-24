771f984f28e6867527959872a5e00af1fc8d7403
Hala Tinn <hala79tinn@gmail.com>
2022-04-24 18:16:03 +0200

    Updated scripts.js
    Corrected loops after affected from newly added IIFE.

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@ let pokemonRepository = (function () {
         height: 1.6,
         type: ['electric', 'grass', 'dragon', 'ghost', 'bug']
     }
-]
+];
 
 
 // Return all Pokemons
@@ -45,26 +45,11 @@ function add (pokemon) {
    add: add
  };
 
+
 })();
-
-
 
 //Displays Pokemon list with properties
 
-pokemonList.forEach(function getAll(pokemonList) {
-  document.write(pokemonList.name + '<br>'+ 'Height: ' + pokemonList.height + '<br>' + 'Type: ' + pokemonList.type + '<p>')
+pokemonRepository.getAll().forEach(function getAll(pokemonList) {
+  document.write(pokemonList.name + '<br>'+ 'Height: ' + pokemonList.height + '<br>' + 'Type: ' + pokemonList.type + '<p>');
 });
-
-
-
-//Conditionals within loop evaluate height of each Pokemon
-
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1.5 && pokemonList[i].name === 'Slowbro') {
-        console.log(pokemonList[i].name + '- Wow, this is a big Pokemon!');
-    } else if (pokemonList[i].height > 0.5 && pokemonList[i].height < 1.5) {
-        console.log(pokemonList[i].name + '- This is an average size Pokemon.');
-    } else {
-        console.log(pokemonList[i].name + '- This is a small Pokemon.');
-    }
-}
